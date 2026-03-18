8a95e88e1e8277eb36e92e48c8ec464a4e569084
revenueratiopizza <miller64@sheridancollege.ca>
2026-03-18 09:30:13 -0400

    Cleaned up grower script

--- Assets/Scripts/Grower.cs
+++ Assets/Scripts/Grower.cs
@@ -3,18 +3,25 @@ using UnityEngine;
 
 public class Grower : MonoBehaviour
 {
-    public Transform tree;   // First initializing the transform functions for objects
-    public Transform apple;  // so we can use them to make them transform and grow
+    public Transform tree;  // First initializing the transform functions for objects
+    public Transform apple; // so we can use them to make them transform and grow
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        tree.localScale = Vector2.zero;    // Set tree and apple localScale in code instead of in the editor
-        apple.localScale = Vector2.zero;   // so we can easily edit it if needed
+        tree.localScale = Vector2.zero;  // Set tree and apple localScale in code instead of in the editor
+        apple.localScale = Vector2.zero; // so we can easily edit it if needed
     }
-
+    public void StartGrow()
+    {
+        StartCoroutine(GrowTree());  // This can also be done in quotes as long as you spell it EXACTLY right
+        StartCoroutine(GrowApple()); // It's always better to make sure you have a safety net when you do this stuff
+    }
 
     IEnumerator GrowTree()
     {
+        Debug.Log("Started growing Tree");
+        tree.localScale = Vector2.zero;  // Setting size before the coroutine
+        apple.localScale = Vector2.zero; // so the size isn't dumb huge or anything
         float t = 0;
         while (t < 1)
         {
@@ -22,7 +29,24 @@ public class Grower : MonoBehaviour
             tree.localScale = Vector2.one * t; // Vector2.one is the same as setting the scale to 1.0, 1.0, but now it's multiplied by time
             yield return null; // Yield statement prevents the coroutine from looping forever, it's like a bookmark
         }
+        Debug.Log("Finished growing Tree");
     }
+
+    IEnumerator GrowApple()
+    {
+        Debug.Log("Started growing Apple");
+        tree.localScale = Vector2.zero;    // Setting size before the coroutine
+        apple.localScale = Vector2.zero;   // so the size isn't dumb huge or anything
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime; // This is to make the apple grow over time using deltaTime
+            apple.localScale = Vector2.one * t; // Vector2.one is the same as setting the scale to 1.0, 1.0, but now it's multiplied by time
+            yield return null; // Yield statement prevents the coroutine from looping forever, it's like a bookmark
+        }
+        Debug.Log("Finished growing Apple");
+    }
+
     // Update is called once per frame
     void Update()
     {
