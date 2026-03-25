19c0e1da908625cf98aa7f3e85aa65a59af71f2a
revenueratiopizza <miller64@sheridancollege.ca>
2026-03-25 11:00:35 -0400

    I did like nothing

---
+++ Assets/ChudMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ChudMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public Vector2 movement;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3)movement * speed * Time.deltaTime;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+}
