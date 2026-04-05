1591a48c8005f00f52f40450769ce4884505128d
mredkcm <miller64@sheridancollege.ca>
2026-04-05 15:18:27 -0400

    Added mouse detection for firing

--- Assets/Mauga/Scripts/Fire.cs
+++ Assets/Mauga/Scripts/Fire.cs
@@ -21,8 +21,34 @@ public class Fire : MonoBehaviour
     {
         // If left click is checked
         // Then turn off gunny neutral and turn on gunny fire
+        // Else turn on gunny neutral and turn off gunny fire
+
+        if (Input.GetMouseButton(0))
+        {
+            lNeut.SetActive(false);
+            lFire.SetActive(true);
+        }
+        else
+        {
+            lNeut.SetActive(true);
+            lFire.SetActive(false);
+        }
 
         // If right click is checked
         // Then turn off cha cha neutral and turn on cha cha fire
+        // Else turn on cha cha neutral and turn off cha cha fire
+
+        if (Input.GetMouseButton(0))
+        {
+            rNeut.SetActive(false);
+            rFire.SetActive(true);
+        }
+        else
+        {
+            rNeut.SetActive(true);
+            rFire.SetActive(false);
+        }
+
+        // Took from https://docs.unity3d.com/6000.3/Documentation/ScriptReference/Input.GetMouseButton.html
     }
 }
